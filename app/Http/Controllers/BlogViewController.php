9e210a5dd04b4f4a0b2fab6095f51a551d27c573
froide-nobutsugu <s-nobutsugu@froide.co.jp>
2022-02-15 11:44:42 +0900

    13 タイトル一覧

--- app/Http/Controllers/BlogViewController.php
+++ app/Http/Controllers/BlogViewController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogViewController extends Controller
 {
     
     public function index()
     {
-        return 'test';
+        $blogs = Blog::get();
+        return view('index', compact('blogs'));
     }
 }
--- tests/Feature/Controller/BlogViewControllerTest.php
+++ tests/Feature/Controller/BlogViewControllerTest.php
@@ -5,19 +5,36 @@ namespace Tests\Feature\Controller;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Blog;
 
 class BlogViewControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
     /** @test */
     function TOPページを開く()
     {
-        $response = $this->get('/');
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
 
-        $response->assertStatus(200);
+        $this->get('/')
+                ->assertOk()
+                ->assertSee($blog1->title)
+                ->assertSee($blog2->title)
+                ->assertSee($blog3->title);
+
+        /**
+         * タイトルの上書き更新も可能
+         */
+        // $blog1 = Blog::factory()->create(['title' => 'あああ']);
+        // $blog2 = Blog::factory()->create(['title' => 'aaaaa']);
+        // $blog3 = Blog::factory()->create(['title' => '111111']);
+
+        // $this->get('/')
+        //         ->assertOk()
+        //         ->assertSee('あああ')
+        //         ->assertSee('aaaaa')
+        //         ->assertSee('111111');
+        
     }
 }
