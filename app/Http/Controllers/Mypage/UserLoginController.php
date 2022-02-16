00bb8fcd72ef44eb20c575cabf83513e5fc65a9d
froide-nobutsugu <s-nobutsugu@froide.co.jp>
2022-02-16 17:24:00 +0900

    27 ログイン機能

--- app/Http/Controllers/Mypage/UserLoginController.php
+++ app/Http/Controllers/Mypage/UserLoginController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Mypage;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 
 class UserLoginController extends Controller
 {
@@ -15,9 +16,15 @@ class UserLoginController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'email' => ['required', 'email:filter'],
             'password' => ['required'],
         ]);
+
+        if (! auth()->attempt($data)) {
+            throw ValidationException::withMessages(['email' => 'メールアドレスかパスワードが間違っています。']);
+        }
+
+        return redirect('mypage/blogs');
     }
 }
--- tests/Feature/Controller/Mypage/UserLoginControllerTest.php
+++ tests/Feature/Controller/Mypage/UserLoginControllerTest.php
@@ -2,13 +2,15 @@
 
 namespace Tests\Feature\Controllers\Mypage;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /** @see  App\Http\Controllers\Mypage\UserLoginController */
 class UserLoginControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
     /** @test index */
     public function ログイン画面が表示できる()
@@ -30,5 +32,91 @@ class UserLoginControllerTest extends TestCase
         $this->post($url, ['email' => 'ああああ@ccc'])->assertSessionHasErrors(['email' => 'メールアドレスには、有効なメールアドレスを指定してください。']);
 
         $this->post($url, ['password' => ''])->assertSessionHasErrors(['password' => 'パスワードは必ず指定してください。']);
+    }
+
+    /** @test */
+    function ログイン可能()
+    {
+        $postData = [
+            'email' => 'aaa@bbc.ccc.net',
+            'password' => 'test12345',
+        ];
+
+        $dbData = [
+            'email' => 'aaa@bbc.ccc.net',
+            'password' => bcrypt('test12345'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $this->post('mypage/login', $postData)
+            ->assertRedirect('mypage/blogs');
+
+        $this->assertAuthenticatedAs($user);
+
+    }
+
+    /** @test */
+    function ID間違いのためログイン不可()
+    {
+        $postData = [
+            'email' => 'aaa@bbc.ccc.net',
+            'password' => 'test12345',
+        ];
+
+        $dbData = [
+            'email' => 'zzz@bbc.ccc.net',
+            'password' => bcrypt('test12345'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $url = 'mypage/login';
+
+        $this->from($url)
+            ->post($url, $postData)
+            ->assertRedirect($url);
+
+        $this->get($url)
+            ->assertSee('メールアドレスかパスワードが間違っています。');
+
+        // $this->from($url)
+        //     ->followingRedirects()
+        //     ->post($url, $postData)
+        //     ->assertSee('メールアドレスが間違っています。')
+        //     // ->assertSee('<h1>ログイン画面</h1>', false);
+        //     ->assertSeeText('ログイン画面');
+    }
+
+        /** @test */
+    function パスワード間違いのためログイン不可()
+    {
+        $postData = [
+            'email' => 'aaa@bbc.ccc.net',
+            'password' => 'test9999',
+        ];
+
+        $dbData = [
+            'email' => 'aaa@bbc.ccc.net',
+            'password' => bcrypt('test12345'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $url = 'mypage/login';
+
+        $this->from($url)
+            ->post($url, $postData)
+            ->assertRedirect($url);
+
+        $this->get($url)
+            ->assertSee('メールアドレスかパスワードが間違っています。');
+
+        // $this->from($url)
+        //     ->followingRedirects()
+        //     ->post($url, $postData)
+        //     ->assertSee('メールアドレスが間違っています。')
+        //     // ->assertSee('<h1>ログイン画面</h1>', false);
+        //     ->assertSeeText('ログイン画面');
     }
 }
