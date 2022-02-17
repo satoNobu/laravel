6acaa8c1763a026ba94e6799c558e6b479b10fa2
froide-nobutsugu <s-nobutsugu@froide.co.jp>
2022-02-17 13:18:06 +0900

    29 マイページ.

---
+++ tests/Feature/Controller/Mypage/BlogMypageControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Controllers\Mypage;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BlogMypageControllerTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test index */
+    function 認証している場合に限り、マイページを開ける()
+    {
+        // 認証していない場合
+        $this->get('mypage/blogs')
+            ->assertRedirect('mypage/login');
+
+        // 認証済みの場合
+        // $user = User::factory()->create();
+
+        // $this->actingAs($user)->get('mypage/blogs')
+        //     ->assertOk();
+
+        $this->login();
+
+        $this->get('mypage/blogs')
+            ->assertOk();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function login(User $user = null)
+    {
+        // $user = $user ?? User::factory()->create();
+        $user = User::factory()->create();
+
+        // 指定ユーザーを現在のユーザーとして認証する
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
